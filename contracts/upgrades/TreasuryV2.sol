da57f4644bfa55fdcf777969ea2eefa8173a61ce
nebula-labs-xyz <alexei@nebula-labs.xyz>
2025-02-17 18:30:26 -0800

    update

--- contracts/upgrades/TreasuryV2.sol
+++ contracts/upgrades/TreasuryV2.sol
@@ -70,8 +70,8 @@ contract TreasuryV2 is
             _grantRole(DEFAULT_ADMIN_ROLE, guardian);
             _grantRole(MANAGER_ROLE, timelock);
 
-            _start = SafeCast.toUint64(block.timestamp - 219 days);
-            _duration = SafeCast.toUint64(1095 days + 219 days);
+            _start = SafeCast.toUint64(block.timestamp - 180 days);
+            _duration = SafeCast.toUint64(1095 days);
             version++;
             emit Initialized(msg.sender);
         } else {
@@ -101,6 +101,7 @@ contract TreasuryV2 is
      */
     function release(address to, uint256 amount) external nonReentrant whenNotPaused onlyRole(MANAGER_ROLE) {
         uint256 vested = releasable();
+        if (to == address(0)) revert CustomError({msg: "ZERO_ADDRESS"});
         if (amount > vested) revert CustomError({msg: "NOT_ENOUGH_VESTED"});
         _released += amount;
         emit EtherReleased(to, amount);
@@ -116,6 +117,7 @@ contract TreasuryV2 is
      */
     function release(address token, address to, uint256 amount) external whenNotPaused onlyRole(MANAGER_ROLE) {
         uint256 vested = releasable(token);
+        if (to == address(0)) revert CustomError({msg: "ZERO_ADDRESS"});
         if (amount > vested) revert CustomError({msg: "NOT_ENOUGH_VESTED"});
         _erc20Released[token] += amount;
         emit ERC20Released(token, to, amount);
